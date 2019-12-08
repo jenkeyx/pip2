c368df37f976d3c4b75d2f281dc2b61a2fd928d2
Сергей Мхитарян <jenkeyx@iMac-Sergej.local>
2019-12-08 04:43:50 +0300

    Набросок сервлетов, ControllerServlet почти готов. Осталось разобраться с бинами и допилить AreaCheckServlet

--- src/ControllerServlet.java
+++ src/ControllerServlet.java
@@ -1,14 +1,27 @@
-
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.crypto.dom.DOMCryptoContext;
+import java.io.IOException;
 import java.util.Map;
 
 @WebServlet(name = "ControllerServlet", urlPatterns = "controllerServlet")
 public class ControllerServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        if (hasParams(request)){
+            if (hasRequiredParams(request)){
+                if (hasCorrectParams(request)){
+                    request.getRequestDispatcher("WEB-INF/areaCheckServlet").forward(request,response);
+                }
+                else {
+                    //todo ErrorBean
+                    request.getRequestDispatcher("WEB-INF/error.jsp");
+                }
+            }
+        }else {
+            response.sendRedirect("index.jsp");
+        }
     }
 
     private boolean hasParams(HttpServletRequest request){
@@ -19,15 +32,15 @@ public class ControllerServlet extends HttpServlet {
         return paramsMap.containsKey("checkbox[]") && paramsMap.containsKey("y") && paramsMap.containsKey("r");
     }
     private boolean hasCorrectParams(HttpServletRequest request){
-        return isCkeckboxCorrect(request)&&isTextAreaCorrect(request);
+        return isCheckboxCorrect(request)&&isTextAreaCorrect(request);
     }
 
-    private boolean isCkeckboxCorrect(HttpServletRequest request){
+    private boolean isCheckboxCorrect(HttpServletRequest request){
         boolean result = false;
         String[] xArr= request.getParameterValues("checkbox[]");
         for ( String xStr : xArr) {
             if (isNaN(xStr)){
-                Double x = Double.parseDouble(xStr);
+                double x = Double.parseDouble(xStr);
                 if (x > -2 && x < 2 ){
                     result = true;
                 }
@@ -39,7 +52,7 @@ public class ControllerServlet extends HttpServlet {
         boolean result = false;
         String yStr = request.getParameter("y");
         if (isNaN(yStr)){
-           Double y =  Double.parseDouble(yStr);
+           double y =  Double.parseDouble(yStr);
            if (y>=-3 && y<=5){
                result = true;
            }
